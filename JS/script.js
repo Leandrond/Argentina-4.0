bc1ff1024e823934eceb6d03365ffdbee07e602c
Leandrond <113318831+Leandrond@users.noreply.github.com>
2023-02-17 06:44:01 -0300

    form2.0

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,19 @@
-function myFunction() {
-    document.getElementById("lola").innerHTML = "Hello JavaScript!";
-}
+const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+  
